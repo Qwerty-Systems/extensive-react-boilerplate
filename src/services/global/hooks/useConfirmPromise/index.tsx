5817e854ab797cf5e731659b7e23ebc55d42d166
r-o-k-u <corneliusokaya@gmail.com>
2025-06-11 21:01:13 +0300

    feat(layout): layout overhaul

--- src/services/global/hooks/useConfirmPromise/index.tsx
+++ src/services/global/hooks/useConfirmPromise/index.tsx
@@ -1,30 +1,30 @@
-import { Dispatch, useCallback, useMemo, useRef } from "react";
+// import { Dispatch, useCallback, useMemo, useRef } from "react";
 
-export const useConfirmPromise = <T,>() => {
-  const resolveRef = useRef<Dispatch<any>>();
-  const rejectRef = useRef<Dispatch<any>>();
+// export const useConfirmPromise = <T,>() => {
+//   const resolveRef = useRef<Dispatch<any>>();
+//   const rejectRef = useRef<Dispatch<any>>();
 
-  const resolve = useCallback(<Options extends T>(value: Options) => {
-    if (resolveRef.current) {
-      resolveRef.current(value);
-    }
-  }, []);
+//   const resolve = useCallback(<Options extends T>(value: Options) => {
+//     if (resolveRef.current) {
+//       resolveRef.current(value);
+//     }
+//   }, []);
 
-  const reject = useCallback((reason?: any) => {
-    if (rejectRef.current) {
-      rejectRef.current(reason);
-    }
-  }, []);
+//   const reject = useCallback((reason?: any) => {
+//     if (rejectRef.current) {
+//       rejectRef.current(reason);
+//     }
+//   }, []);
 
-  const promise = useCallback(<Options extends T>() => {
-    return new Promise<Options>((resolve, reject) => {
-      resolveRef.current = resolve;
-      rejectRef.current = reject;
-    });
-  }, []);
+//   const promise = useCallback(<Options extends T>() => {
+//     return new Promise<Options>((resolve, reject) => {
+//       resolveRef.current = resolve;
+//       rejectRef.current = reject;
+//     });
+//   }, []);
 
-  return useMemo(
-    () => ({ promise, resolve, reject }),
-    [promise, resolve, reject]
-  );
-};
+//   return useMemo(
+//     () => ({ promise, resolve, reject }),
+//     [promise, resolve, reject]
+//   );
+// };
--- src/services/social-auth/social-auth.tsx
+++ src/services/social-auth/social-auth.tsx
@@ -5,17 +5,17 @@ import FacebookAuth from "./facebook/facebook-auth";
 import { isFacebookAuthEnabled } from "./facebook/facebook-config";
 import GoogleAuth from "./google/google-auth";
 import { isGoogleAuthEnabled } from "./google/google-config";
-import KeycloakAuth from "./keycloak/keycloak-auth";
-import { isKeycloakAuthEnabled } from "./keycloak/keycloak-config";
+// import KeycloakAuth from "./keycloak/keycloak-auth";
+//import { isKeycloakAuthEnabled } from "./keycloak/keycloak-config";
 
 export default function SocialAuth() {
   return (
     <Grid container spacing={2}>
-      {isKeycloakAuthEnabled && (
+      {/* {isKeycloakAuthEnabled && (
         <Grid sx={{ xs: 12 }}>
           <KeycloakAuth />
         </Grid>
-      )}
+      )} */}
       {isGoogleAuthEnabled && (
         <Grid sx={{ xs: 12 }}>
           <GoogleAuth />
